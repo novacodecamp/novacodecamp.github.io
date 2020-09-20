b97bdb3563f3a0a37e73d80805ab7fed4b634085
Lee Richardson <github@leerichardson.com>
2020-09-20 13:15:54 -0400

    Added twitter card meta-tags and title

--- Angular/src/app/speaker/speaker.component.ts
+++ Angular/src/app/speaker/speaker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { SessionizeService, Speaker } from '../sessionize-service.service';
 
@@ -11,13 +12,35 @@ export class SpeakerComponentComponent implements OnInit {
   public speakerId: string;
   public speaker: Speaker;
 
-  constructor(private route: ActivatedRoute, private sessionizeService: SessionizeService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private sessionizeService: SessionizeService,
+    private metaService: Meta,
+    private titleService: Title
+  ) {}
 
   ngOnInit(): void {
     const speakerId = this.route.snapshot.paramMap.get('id');
     this.speakerId = speakerId;
-    this.sessionizeService.getSessionizeData().subscribe(sessionizeApiResult => {
-      this.speaker = sessionizeApiResult.speakers.find(s => s.id === speakerId);
-    });
+    this.metaService.addTags([
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@novacodecamp' },
+      { name: 'twitter:title', content: `loading...` },
+    ]);
+    this.sessionizeService
+      .getSessionizeData()
+      .subscribe((sessionizeApiResult) => {
+        this.speaker = sessionizeApiResult.speakers.find(
+          (s) => s.id === speakerId
+        );
+        this.titleService.setTitle(`${this.speaker.firstName} ${this.speaker.lastName}`);
+        this.metaService.addTags([
+          { name: 'description', content: `${this.speaker.firstName} ${this.speaker.lastName}: ${this.speaker.tagLine}` },
+          { name: 'robots', content: 'index, follow' },
+          { name: 'twitter:description', content: `${this.speaker.bio}` },
+          { name: 'twitter:image', content: `${this.speaker.profilePicture}` },
+        ]);
+        this.metaService.updateTag({ name: 'twitter:title', content: `${this.speaker.firstName} ${this.speaker.lastName}` }, "name='twitter:title'");
+      });
   }
 }
