705dd8af1661e83e82e27d7feec34e4c7a2e948d
Lee Richardson <github@leerichardson.com>
2020-09-19 23:39:06 -0400

    Fix issues with angular deploy

--- buildapp.ps1
+++ buildapp.ps1
@@ -1,5 +1,5 @@
 Push-Location ./Angular/
 npm install
-ng build --prod --aot
+ng build --prod --aot --base-href "https://novacodecamp.org/app/"
 Pop-Location
-Move-Item ./Angular/dist/* ./app 
+Move-Item -Force ./Angular/dist/* ./app 
